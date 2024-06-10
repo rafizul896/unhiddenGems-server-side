0395eb2a3a970d90577c1ff4314cf8320f209ec9
rafizul896 <rafizulislam896@gmail.com>
2024-06-10 15:42:26 +0600

    tourGuides and packages data get

--- index.js
+++ index.js
@@ -9,35 +9,73 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.y7qmkns.mongodb.net/?retryWrites=true&w=majority&appName=cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    
-    // Send a ping to confirm a successful connection
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+    try {
+        const tourGuidesCollection = client.db('tourist-Guide').collection('tourGuides');
+        const packagesCollection = client.db('tourist-Guide').collection('packages');
+        const wishlistsCollection = client.db('tourist-Guide').collection('wishlist');
+
+        app.get('/tourGuides', async (req, res) => {
+            const result = await tourGuidesCollection.find().toArray();
+            res.send(result);
+        })
+        
+        // get packages collections
+        app.get('/packages', async (req, res) => {
+            const result = await packagesCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.get('/packages/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await packagesCollection.findOne(query);
+            res.send(result);
+        })
+
+        // Wishlist
+        app.get('/wishlist', async (req, res) => {
+            const result = await wishlistsCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.post('/wishlist', async (req, res) => {
+            const package = req.body;
+            const query = { packageId: package.packageId }
+            const isExist = await wishlistsCollection.findOne(query)
+            if (isExist) {
+                return res.send({ message: 'exist' })
+            }
+            const result = await wishlistsCollection.insertOne(package);
+            res.send(result)
+        })
+
+        // Send a ping to confirm a successful connection
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send(`Hello from Tourist Guide server..!`)
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Tourist Guide server is running port on ${port}`)
 })
